9c0eb5c56f7cd0628ac420408d8bdffecf982c13
Phil_McNamara <phil.j.mcnamara@gmail.com>
2021-02-24 16:12:25 +0100

    fixed logic for sending approved order reminder emails

--- beyond_django/approved_order_reminder.py
+++ beyond_django/approved_order_reminder.py
@@ -6,22 +6,20 @@ from django.urls import reverse
 
 from order_management.models import Order
 
-orders = Order.objects.all()
-
 # if there are approved orders
 try:
-    orders.get(status="Approved")
-    email_text = '''Dear Order Manager,
+    if len(Order.objects.all().filter(status="approved")) > 0:
+        email_text = '''Dear Order Manager,
 
-There are approved orders pending in the database. Please place the orders at your earliest convenience.
-    
-You can find them here: https://{}:/order_management/order/?q-l=on&q=status+%3D+%22approved%22+'''.format(ALLOWED_HOSTS[0])
+    There are approved orders pending in the database. Please place the orders at your earliest convenience.
+        
+    You can find them here: https://{}:/order_management/order/?q-l=on&q=status+%3D+%22approved%22+'''.format(ALLOWED_HOSTS[0])
 
-    send_mail(
-        subject="Approved orders are pending",
-        message=email_text,
-        from_email=SERVER_EMAIL_ADDRESS,
-        recipient_list=ORDER_MANAGER_EMAIL_ADDRESSES
+        send_mail(
+            subject="Approved orders are pending",
+            message=email_text,
+            from_email=SERVER_EMAIL_ADDRESS,
+            recipient_list=ORDER_MANAGER_EMAIL_ADDRESSES
     )
 
 except:
