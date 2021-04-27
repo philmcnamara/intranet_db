fcde6f0dfae0c4af3b5e507c8704f3d173659088
Phil_McNamara <phil.j.mcnamara@gmail.com>
2021-04-27 16:58:26 +0200

    fixed error when record no longer exists for order to be cancelled

--- order_management/admin.py
+++ order_management/admin.py
@@ -413,8 +413,8 @@ def change_order_status_to_cancelled(modeladmin, request, queryset):
             
             message = inspect.cleandoc(message)
             send_mail('Order Request Cancelled', message, SERVER_EMAIL_ADDRESS, [recipient.email] ,fail_silently=False,)
-            record = RecordToBeApproved.objects.all().get(object_id=order.id)
-            record.delete()
+            #record = RecordToBeApproved.objects.all().get(object_id=order.id)
+            #record.delete()
             order.save()
 change_order_status_to_cancelled.short_description = "Change STATUS of selected to CANCELLED"
 change_order_status_to_cancelled.acts_on_all = True
